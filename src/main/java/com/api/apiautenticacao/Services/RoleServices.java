3b293b94aff9b070beb62803cb6c21a8431d0567
joaogadev <joaogabriel.trindade04@gmail.com>
2026-05-14 12:20:02 -0300

    Regras de Serviço dos Cargos

--- src/main/java/com/api/apiautenticacao/Services/RoleServices.java
+++ src/main/java/com/api/apiautenticacao/Services/RoleServices.java
@@ -4,8 +4,15 @@ import com.api.apiautenticacao.Model.RolesModel;
 import com.api.apiautenticacao.repository.RolesRepository;
 
 public class RoleServices {
-    private RolesModel role;
-    private RolesRepository repo;
+    private final RolesRepository repo;
+
+    public RoleServices(RolesRepository repo) {
+        this.repo = repo;
+    }
+
+    public RolesModel getRole(String name) {
+        return repo.findByName(name).orElseThrow(() -> new RuntimeException("Role not found: " + name));
+    }
 
 
 }
---
+++ src/main/java/com/api/apiautenticacao/Services/TokenServices.java
@@ -0,0 +1,4 @@
+package com.api.apiautenticacao.Services;
+
+public class TokenServices {
+}
